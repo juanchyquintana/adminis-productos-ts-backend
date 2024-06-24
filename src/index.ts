f0bc4ed1a5d054dbca736f68a403bc7aae3403b0
Juan Diego <quintanajuanchy25@gmail.com>
2024-06-24 15:48:30 -0300

    se comenzó a crear los controladores para el modelo products

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import colors from 'colors'
 import server from "./server";
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 server.listen(4000, () => {
     console.log(colors.cyan.bold(`REST API en el puerto ${port}`))
--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
+import { createProduct } from "../controllers/product";
 
 const router = Router()
 
 router.get('/', (req, res) => {
     res.json('Hooola')
 })
+
+router.post('/', createProduct)
 
 export default router;
