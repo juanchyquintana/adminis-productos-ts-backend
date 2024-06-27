dba9025eb9520224ef661eb4d4bb7c8f17ded4bc
Juan Diego <quintanajuanchy25@gmail.com>
2024-06-27 10:35:24 -0300

    se terminaron los endpoints del crud en el controller de product

--- src/controllers/product.ts
+++ src/controllers/product.ts
@@ -10,10 +10,59 @@ export const getProduct = async (req: Request, res: Response) => {
   }
 };
 
+export const getProductById = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const product = await Proudct.findByPk(id);
+
+    if (!product) {
+      return res.status(404).json({ error: "Producto No Encontrado" });
+    }
+
+    res.json({ data: product });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 export const createProduct = async (req: Request, res: Response) => {
   try {
     const product = await Proudct.create(req.body);
     res.status(201).json({ data: product });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const updateProduct = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const product = await Proudct.findByPk(id);
+
+    if (!product) {
+      return res.status(404).json({ error: "Producto No Encontrado" });
+    }
+
+    await product.update(req.body);
+    await product.save();
+
+    res.json({ data: product });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const deleteProduct = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const product = await Proudct.findByPk(id);
+
+    if (!product) {
+      return res.status(404).json({ error: "Producto No Encontrado" });
+    }
+
+    await product.destroy();
+    res.json({ data: "Producto Eliminado" });
   } catch (error) {
     console.log(error);
   }
--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,11 +1,20 @@
-import { createProduct, getProduct } from "../controllers/product";
 import { Router } from "express";
-
+import {
+  createProduct,
+  deleteProduct,
+  getProduct,
+  getProductById,
+  updateProduct,
+} from "../controllers/product";
 import productValidator from "../validator/productValidator";
+import paramsValidator from "../validator/paramsValidator";
 
 const router = Router();
 
 router.get("/", getProduct);
+router.get("/:id", paramsValidator, getProductById);
 router.post("/", productValidator, createProduct);
+router.put("/:id", paramsValidator, productValidator, updateProduct);
+router.delete("/:id", paramsValidator, deleteProduct);
 
 export default router;
