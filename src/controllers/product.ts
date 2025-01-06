dd576698b42420b415a40f4c4374f35e1c6b113f
Juan Diego <quintanajuanchy25@gmail.com>
2025-01-06 19:04:42 -0300

    delete file validator and finished crud of products

--- src/controllers/product.ts
+++ src/controllers/product.ts
@@ -1,10 +1,29 @@
-import Proudct from "../models/Product.model";
+import Product from "../models/Product.model";
 import { Request, Response } from "express";
 
 export const getProduct = async (req: Request, res: Response) => {
   try {
-    const products = await Proudct.findAll();
+    const products = await Product.findAll({
+      order: [["price", "DESC"]],
+    });
     res.status(200).json({ data: products });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getProductById = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByPk(id);
+
+    if (!product) {
+      return res.status(404).json({
+        error: "Producto No Encontrado",
+      });
+    }
+
+    res.json({ data: product });
   } catch (error) {
     console.log(error);
   }
@@ -12,9 +31,53 @@ export const getProduct = async (req: Request, res: Response) => {
 
 export const createProduct = async (req: Request, res: Response) => {
   try {
-    const product = await Proudct.create(req.body);
+    const product = await Product.create(req.body);
     res.status(201).json({ data: product });
   } catch (error) {
     console.log(error);
   }
 };
+
+export const updateProduct = async (req: Request, res: Response) => {
+  const { id } = req.params
+  const product = await Product.findByPk(id)
+
+  if(!product) {
+      return res.status(404).json({
+          error: 'Producto No Encontrado'
+      })
+  }
+  
+  await product.update(req.body)
+  await product.save()
+  res.json({data: product})
+}
+
+export const updateAvailability = async (req: Request, res: Response) => {
+  const { id } = req.params
+  const product = await Product.findByPk(id)
+
+  if(!product) {
+      return res.status(404).json({
+          error: 'Producto No Encontrado'
+      })
+  }
+
+  product.availability = !product.dataValues.availability
+  await product.save()
+  res.json({data: product})
+}
+
+export const deleteProduct = async (req: Request, res: Response) => {
+  const { id } = req.params
+  const product = await Product.findByPk(id)
+
+  if(!product) {
+      return res.status(404).json({
+          error: 'Producto No Encontrado'
+      })
+  }
+  
+  await product.destroy()
+  res.json({data: 'Producto Eliminado'})
+}
--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,11 +1,66 @@
-import { createProduct, getProduct } from "../controllers/product";
+import { createProduct, deleteProduct, getProduct, getProductById, updateAvailability, updateProduct } from "../controllers/product";
 import { Router } from "express";
-
-import productValidator from "../validator/productValidator";
+import { body, param } from "express-validator";
+import { handleInputErrors } from "../middleware";
 
 const router = Router();
 
 router.get("/", getProduct);
-router.post("/", productValidator, createProduct);
+router.get(
+  "/:id",
+  param("id").isInt().withMessage("ID no válido"),
+  handleInputErrors,
+  getProductById
+);
+
+router.post(
+  "/",
+  body("name")
+    .notEmpty()
+    .withMessage("El nombre de Producto no puede ir vacio"),
+  body("price")
+    .isNumeric()
+    .withMessage("Valor no válido")
+    .notEmpty()
+    .withMessage("El precio de Producto no puede ir vacio")
+    .custom((value) => value > 0)
+    .withMessage("Precio no válido"),
+  handleInputErrors,
+  createProduct
+);
+
+router.put(
+  "/:id",
+  param("id").isInt().withMessage("ID no válido"),
+  body("name")
+    .notEmpty()
+    .withMessage("El nombre de Producto no puede ir vacio"),
+  body("price")
+    .isNumeric()
+    .withMessage("Valor no válido")
+    .notEmpty()
+    .withMessage("El precio de Producto no puede ir vacio")
+    .custom((value) => value > 0)
+    .withMessage("Precio no válido"),
+  body("availability")
+    .isBoolean()
+    .withMessage("Valor para disponibilidad no válido"),
+  handleInputErrors,
+  updateProduct
+);
+
+router.patch(
+  "/:id",
+  param("id").isInt().withMessage("ID no válido"),
+  handleInputErrors,
+  updateAvailability
+);
+
+router.delete(
+  "/:id",
+  param("id").isInt().withMessage("ID no válido"),
+  handleInputErrors,
+  deleteProduct
+);
 
 export default router;
